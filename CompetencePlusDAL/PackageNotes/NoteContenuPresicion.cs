7998ff4df7fc89adcd12251ecdda36a47f62cab9
HamzaZbaakh <hamza.zbaakh@gmail.com>
2015-01-04 14:19:38 +0000

    Class NoteContenuPresicion

---
+++ CompetencePlusDAL/PackageNotes/NoteContenuPresicion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompetencePlus.PackageNotes
+{
+    class NoteContenuPresicion
+    {
+             private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private PackageStagiaires.Stagiaire stagiaire;
+
+        public PackageStagiaires.Stagiaire Stagiaire
+        {
+            get { return stagiaire; }
+            set { stagiaire = value; }
+        }
+        private float valeur;
+
+        public float Valeur
+        {
+            get { return valeur; }
+            set { valeur = value; }
+        }
+        private PackagePrecision.ContenuPresicion contenuPresicion;
+
+        public PackagePrecision.ContenuPresicion ContenuPresicion
+        {
+            get { return contenuPresicion; }
+            set { contenuPresicion = value; }
+        }
+
+        
+        public NoteContenuPresicion() { }
+        public NoteContenuPresicion(int id, PackageStagiaires.Stagiaire stagiaire, float valeur, PackagePrecision.ContenuPresicion contenuPresicion)
+        {
+
+            this.id = Id;
+            this.stagiaire = Stagiaire;
+            this.valeur = Valeur;
+            this.ContenuPresicion = ContenuPresicion;
+        
+        }
+
+    }
+}
